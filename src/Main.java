186e69c3d1b9d8f9ba2632d603e4c31a254c64f3
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-05-02 21:24:39 +0300

    Class and pacages created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public class Main {
+        public static void main(String[] args) {
+            Employee employee = new Employee(" Rudenko", "Olga ", " Gennadiivna", " Java developer", " olga.altexsoft@com ", "7257823 ", 29);
+
+        }
     }
-}
---
+++ src/net/hillel/homework2/IntroductionToOop/same/SameName.java
@@ -0,0 +1,4 @@
+package net.hillel.homework2.IntroductionToOop.same;
+
+public class SameName {
+}
